18915e01859f4b944da89e1e0abe21875c359cf0
jahaknight <jahaknight1@gmail.com>
2025-11-03 15:48:36 -0500

    Initial Spring Boot project

---
+++ src/main/java/com/heshima/heshima_studio/HeshimaStudioApplication.java
@@ -0,0 +1,13 @@
+package com.heshima.heshima_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeshimaStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeshimaStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heshima/heshima_studio/HeshimaStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heshima.heshima_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeshimaStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
